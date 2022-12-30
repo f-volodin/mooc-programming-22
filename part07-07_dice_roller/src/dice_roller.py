0c7013f739b6713a9de549720c966fef3dd3fa4c
Volodin F. V <volodin_filipp@yahoo.com>
2022-12-30 16:54:13 +0200

    p. 7
    exercise added

--- part07-07_dice_roller/src/dice_roller.py
+++ part07-07_dice_roller/src/dice_roller.py
@@ -1,1 +1,25 @@
-# Write your solution here
+from random import randint
+
+def roll(die: str):
+    if die == "A":
+        lister = [3, 3, 3, 3, 3, 6]
+    elif die == "B":
+        lister = [2, 2, 2, 5, 5, 5]
+    elif die == "C":
+        lister = [1, 4, 4, 4, 4, 4]
+    return(lister[randint(0,5)])
+    
+def play(die1: str, die2: str, times: int):
+    n = 0
+    m = 0
+    r = 0
+    for i in range (0, times):
+        chislo_1 = roll(die1)
+        chislo_2 = roll(die2)
+        if chislo_1 > chislo_2:
+            n = n + 1
+        elif chislo_1 < chislo_2:
+            m = m + 1
+        elif chislo_1 == chislo_2:
+            r = r + 1
+    return (n, m, r)
